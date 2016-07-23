466442cac48b732238f0eab751bce0918142e9bc
ghsy158 <fgh326@126.com>
2016-07-23 23:04:48 +0800

    add

--- src/main/java/com/fgh/test/Consumer.java
+++ src/main/java/com/fgh/test/Consumer.java
@@ -4,13 +4,20 @@ import java.io.IOException;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Provider {
+import com.fgh.entity.User;
+import com.fgh.service.UserService;
+
+public class Consumer {
 
 	public static void main(String[] args) throws IOException {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-provider.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo-consumer.xml");
 	
 		context.start();
 		System.out.println("服务已启动...");
+		
+		UserService userService = (UserService) context.getBean("userService");
+		User user = userService.getUser();
+		System.out.println(user);
 		System.in.read();//为了保证服务一直开着，利用输入流的阻塞来模拟
 	}
 }
